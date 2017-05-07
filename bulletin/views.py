2bf13ca2e17af32fd15f782ad71c1f4c46c5d790
Yash Agrawal <yagrawal900@gmail.com>
2017-05-08 03:45:37 +0530

    used Decorator

--- bulletin/views.py
+++ bulletin/views.py
@@ -7,6 +7,7 @@ from django.urls import reverse
 from django.template import loader
 from django.shortcuts import render
 from datetime import datetime
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     if request.user.is_authenticated:
@@ -29,99 +30,78 @@ def index(request):
 def logout(request):
     logout(request)
     return render(request, 'bulletin/logout.html')
+
+@login_required(login_url='/bulletin/')
 def notice(request):
-	if request.user.is_authenticated:
-	    latest_notice_list = Notice.objects.order_by('-upload')
-	    template = loader.get_template('bulletin/notices.html')
-	    context = {
-	        'latest_notice_list': latest_notice_list,
-	    }
-	    return HttpResponse(template.render(context, request))
-	else:
-	    form = Login()
-        return render(request, 'bulletin/index.html',{'form':form,})
-
-def notice_detail(request, notice_id):
-	if request.user.is_authenticated:
-	    notice = get_object_or_404(Notice, pk=notice_id)
-	    expirydate= notice.expiry_date
-	    if datetime.today() > expirydate:
-	        status = 'expired'
-	    else:
-	        status= 'active'
-	    return render(request, 'bulletin/notice_detail.html', {'notice': notice, 'status':status})
-	else:
-	    form= Login()
-        return render(request, 'bulletin/index.html',{'form':form,})
-def student(request):
-	if request.user.is_superuser:
-	     student_list = Student.objects.all().order_by('enr_no')
-	     template = loader.get_template('bulletin/students.html') 
-	     context = {                                           
-	         'student_list': student_list,         
-	     }
-	     return HttpResponse(template.render(context, request))
-	else:
-	    form = Login()
-        return render(request, 'bulletin/index.html',{'form':form,})
-
-def student_detail(request, student_id):
-	if request.user.is_authenticated:
-	    student = get_object_or_404(Student, pk=student_id)
-	    return render(request, 'bulletin/student_detail.html', {'student': student}) 
-	else:
-	    form = Login()
-        return render(request, 'bulletin/index.html',{'form':form,})
-
-def category(request):
-	if request.user.is_authenticated:
-	    category_list = Category.objects.all().order_by('name')
-	    template = loader.get_template('bulletin/categories.html')
-	    context = {
-	        'category_list': category_list,
-	    }
-	    return HttpResponse(template.render(context, request))    
-	else:
-	    form = Login()
-        return render(request, 'bulletin/index.html',{'form':form,})
-
-def category_notice(request,category_id):
-	if request.user.is_authenticated:
-	    category = get_object_or_404(Category, pk=category_id)
-	    notice_list = Notice.objects.all().filter(category = category_id)
-	    template = loader.get_template('bulletin/category_notice.html')
-	    context = {                                           
-	        'notice_list': notice_list,         
-	    }
-	    return HttpResponse(template.render(context, request))
-	else:
-	    form = Login()
-        return render(request, 'bulletin/index.html',{'form':form,})
-
-def subscription(request,student_id):
-    if request.user.is_authenticated:	
-	subscribed_list = Subscription.objects.all().filter(student__id = student_id)
-	template = loader.get_template('bulletin/subscribed.html')
-	context = {                                           
-	    'subscribed_list': subscribed_list,         
+	latest_notice_list = Notice.objects.order_by('-upload')
+	template = loader.get_template('bulletin/notices.html')
+	context = {
+	    'latest_notice_list': latest_notice_list,
 	}
 	return HttpResponse(template.render(context, request))
-    else:
-	form = Login()
-    return render(request, 'bulletin/index.html',{'form':form,})
-
-def starred(request,student_id):
-    if request.user.is_authenticated:
-	notice_list = SNlink.objects.all().filter(student__id = student_id)
-	template = loader.get_template('bulletin/starred.html')
-	context = {                                           
-	    'notice_list': notice_list,         
-	}
-	return HttpResponse(template.render(context, request))
-    else:
-	form = Login()
-    return render(request, 'bulletin/index.html',{'form':form,})
 	
+
+@login_required(login_url='/bulletin/')
+def notice_detail(request, notice_id):
+        notice = get_object_or_404(Notice, pk=notice_id)
+	expirydate= notice.expiry_date
+	if datetime.today() > expirydate:
+	    status = 'expired'
+	else:
+	    status= 'active'
+	return render(request, 'bulletin/notice_detail.html', {'notice': notice, 'status':status})
+	
+@login_required(login_url='/bulletin/')
+def student(request):
+    student_list = Student.objects.all().order_by('enr_no')
+    template = loader.get_template('bulletin/students.html') 
+    context = {                                           
+        'student_list': student_list,         
+    }
+    return HttpResponse(template.render(context, request))
+
+@login_required(login_url='/bulletin/')
+def student_detail(request, student_id):
+    student = get_object_or_404(Student, pk=student_id)
+    return render(request, 'bulletin/student_detail.html', {'student': student}) 
+
+@login_required(login_url='/bulletin/')
+def category(request):
+    category_list = Category.objects.all().order_by('name')
+    template = loader.get_template('bulletin/categories.html')
+    context = {
+        'category_list': category_list,
+    }
+    return HttpResponse(template.render(context, request))    
+
+@login_required(login_url='/bulletin/')
+def category_notice(request,category_id):
+    category = get_object_or_404(Category, pk=category_id)
+    notice_list = Notice.objects.all().filter(category = category_id)
+    template = loader.get_template('bulletin/category_notice.html')
+    context = {                                           
+       'notice_list': notice_list,         
+    }
+    return HttpResponse(template.render(context, request))
+
+
+@login_required(login_url='/bulletin/')
+def subscription(request,student_id):
+    subscribed_list = Subscription.objects.all().filter(student__id = student_id)
+    template = loader.get_template('bulletin/subscribed.html')
+    context = {                                           
+        'subscribed_list': subscribed_list,         
+    }
+    return HttpResponse(template.render(context, request))
+
+@login_required(login_url='/bulletin/')
+def starred(request,student_id):
+    notice_list = SNlink.objects.all().filter(student__id = student_id)
+    template = loader.get_template('bulletin/starred.html')
+    context = {                                           
+        'notice_list': notice_list,         
+    }
+    return HttpResponse(template.render(context, request))	
 
 def register(request):
     if request.method == 'POST':
